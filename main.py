a1dfd97deb48aff512fb4ac53e4ca73e0efe946b
jniyonkuru <jiniyonkurujimmy@gmail.com>
2024-09-08 12:21:41 +0200

    handle user friendly output + input cases handl

--- main.py
+++ main.py
@@ -1,13 +1,15 @@
-user_prompt = "Type add, show or exit"
+user_prompt = "Type add, show or exit: "
 todos = []
 while True:
     user_decision = input(user_prompt)
+    user_decision = user_decision.strip()
     match user_decision:
         case "add":
             todo = input("Enter a todo: ")
             todos.append(todo)
         case "show":
-            print(todos)
+            for item in todos:
+                print(item)
         case "exit":
             break
 print("Bye!")
