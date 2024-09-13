5afe9053e10033856a6df243e535ad57db510adb
jniyonkuru <jiniyonkurujimmy@gmail.com>
2024-09-13 19:14:52 +0200

    remove the unnecessary whitespace

--- main.py
+++ main.py
@@ -20,7 +20,9 @@ while True:
             todos = file.readlines()
             file.close()
 
-            for index, item in enumerate(todos):
+            new_todos = [item.strip('\n') for item in todos]
+
+            for index, item in enumerate(new_todos):
                 print(f"{index + 1}-{item}")
 
         case "edit":
