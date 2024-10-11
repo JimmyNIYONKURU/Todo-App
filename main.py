5b86c84fd2255294c3aff06b132a20b61899ac20
Jimmy NIYONKURU <104285083+JimmyNIYONKURU@users.noreply.github.com>
2024-10-11 22:47:16 +0200

    Improved  'add' feature and optimised the code

--- main.py
+++ main.py
@@ -2,55 +2,53 @@ user_prompt = "Type add, show, edit, complete or exit: "
 while True:
     user_decision = input(user_prompt)
     user_decision = user_decision.strip()
-    match user_decision:
-        case "add":
-            with open("todos.txt","r") as file:
-                todos = file.readlines()
+    if "add" in user_decision:
+        with open("todos.txt", "r") as file:
+            todos = file.readlines()
 
-            todo = input("Enter a todo: ") +"\n"
-            todos.append(todo)
+        todo = user_decision[4:] + "\n"
+        todos.append(todo)
 
-            with open("todos.txt", "w") as file:
-                file.writelines(todos)
-
-
-        case "show":
-            with open("todos.txt","r") as file:
-                todos = file.readlines()
+        with open("todos.txt", "w") as file:
+            file.writelines(todos)
 
 
-            #new_todos = [item.strip('\n') for item in todos]
+    elif "show" in user_decision:
+        with open("todos.txt", "r") as file:
+            todos = file.readlines()
 
-            for index, item in enumerate(todos):
-                item = item.strip('\n')
-                print(f"{index + 1}-{item}")
+        # new_todos = [item.strip('\n') for item in todos]
 
-        case "edit":
-            index_task = int(input("Index of the task to edit: "))
-            with open("todos.txt","r") as file:
-                todos = file.readlines()
+        for index, item in enumerate(todos):
+            item = item.strip('\n')
+            print(f"{index + 1}-{item}")
 
-            new_item = input("Enter a new task: ")
-            todos[index_task - 1] = new_item + '\n'
+    elif "edit" in user_decision:
+        index_task = int(user_decision[5:])
+        with open("todos.txt", "r") as file:
+            todos = file.readlines()
 
-            with open("todos.txt", "w") as file:
-                file.writelines(todos)
+        new_item = input("Enter a new task: ")
+        todos[index_task - 1] = new_item + '\n'
+
+        with open("todos.txt", "w") as file:
+            file.writelines(todos)
 
 
-        case "complete":
-            index_task = int(input("Index of the task to mark as completed: "))
+    elif "complete" in user_decision:
+        index_task = int(user_decision[8:])
 
-            with open("todos.txt","r") as file:
-                todos = file.readlines()
+        with open("todos.txt", "r") as file:
+            todos = file.readlines()
 
-            index = index_task - 1
-            todo_toremove = todos[index].strip('\n')
-            todos.pop()
+        index = index_task - 1
+        todo_toremove = todos[index].strip('\n')
+        todos.pop(index)
 
-            with open("todos.txt", "w") as file:
-                file.writelines(todos)
+        with open("todos.txt", "w") as file:
+            file.writelines(todos)
 
-            print(f"{todo_toremove} was removed from you tasks")
-        case "exit":
-            break
+        print(f"{todo_toremove} was removed from you tasks")
+    elif "exit" in user_decision:
+        break
 print("Bye!")
