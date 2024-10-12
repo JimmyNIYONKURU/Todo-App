f1ac9960a5cc67720327a986505260a9d04e0d41
jniyonkuru <jiniyonkurujimmy@gmail.com>
2024-10-13 00:12:22 +0200

    - Fixed bugs - Anticipated errors

--- main.py
+++ main.py
@@ -2,18 +2,18 @@ user_prompt = "Type add, show, edit, complete or exit: "
 while True:
     user_decision = input(user_prompt)
     user_decision = user_decision.strip()
-    if "add" in user_decision:
+    if user_decision.startswith('add'):
         with open("todos.txt", "r") as file:
             todos = file.readlines()
 
-        todo = user_decision[4:] + "\n"
-        todos.append(todo)
+        todo = user_decision[4:]
+        todos.append(todo + '\n')
 
         with open("todos.txt", "w") as file:
             file.writelines(todos)
 
 
-    elif "show" in user_decision:
+    elif user_decision.startswith('show'):
         with open("todos.txt", "r") as file:
             todos = file.readlines()
 
@@ -23,32 +23,39 @@ while True:
             item = item.strip('\n')
             print(f"{index + 1}-{item}")
 
-    elif "edit" in user_decision:
-        index_task = int(user_decision[5:])
-        with open("todos.txt", "r") as file:
-            todos = file.readlines()
+    elif user_decision.startswith('edit'):
+        try:
+            index_task = int(user_decision[5:])
+            with open("todos.txt", "r") as file:
+                todos = file.readlines()
 
-        new_item = input("Enter a new task: ")
-        todos[index_task - 1] = new_item + '\n'
+            new_item = input("Enter a new task: ")
+            todos[index_task - 1] = new_item + '\n'
 
-        with open("todos.txt", "w") as file:
-            file.writelines(todos)
+            with open("todos.txt", "w") as file:
+                file.writelines(todos)
+        except ValueError:
+            print('Your command is not valid!')
+            continue
 
+    elif user_decision.startswith('complete'):
+        try:
+            index_task = int(user_decision[8:])
 
-    elif "complete" in user_decision:
-        index_task = int(user_decision[8:])
+            with open("todos.txt", "r") as file:
+                todos = file.readlines()
 
-        with open("todos.txt", "r") as file:
-            todos = file.readlines()
+            index = index_task - 1
+            todo_toremove = todos[index].strip('\n')
+            todos.pop(index)
 
-        index = index_task - 1
-        todo_toremove = todos[index].strip('\n')
-        todos.pop(index)
+            with open("todos.txt", "w") as file:
+                file.writelines(todos)
 
-        with open("todos.txt", "w") as file:
-            file.writelines(todos)
-
-        print(f"{todo_toremove} was removed from you tasks")
+            print(f"{todo_toremove} was removed from you tasks")
+        except IndexError:
+            print('Invalid command!')
+            continue
     elif "exit" in user_decision:
         break
 print("Bye!")
