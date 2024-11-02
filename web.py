1cb6a83b67421741cf3aa8703ee36aced7d05310
jniyonkuru <jiniyonkurujimmy@gmail.com>
2024-11-02 12:43:47 +0100

    implented "add new to do" features

--- web.py
+++ web.py
@@ -1,6 +1,12 @@
 import streamlit as st
 import func
 
+def add_to_do():
+    todo = st.session_state["new_todo"] + "\n"
+    todos.append(todo)
+    func.write_todos(todos)
+    print(todo)
+
 todos = func.get_todos()
 
 st.title("To do app")
@@ -9,4 +15,6 @@ st.write("This app is to increase your productivity")
 
 for todo in todos:
     st.checkbox(todo)
-st.text_input(label="", placeholder="Add a new to do...")
+st.text_input(label="New To-do", placeholder="Add a new to do...", on_change= add_to_do, key ="new_todo")
+
+print("hello")
